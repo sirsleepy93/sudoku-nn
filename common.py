f868d26cd4d59b95ee44f219b98b897a57529fc6
sirsleepy93 <sir.sleepy93@gmail.com>
2017-06-28 00:06:37 -0500

    Still placing an int somewhere

--- common.py
+++ common.py
@@ -1,58 +1,45 @@
 import numpy as np
 import itertools as it
 
-"""
-units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
-"""
-
 # Return a list of all indices
 def get_grid(size=3):
 	enum = range(np.square(size))
 	return [g for g in it.product(enum, enum)]
 
-# Return a list of all rows
+
+# Yield a list of all rows
 def row_units(size=3):
 	grid = get_grid(size)
-	#row_units = []
 	for i in range(np.square(size)):
-		#row_units.append([idx for idx in grid if idx[1] == i])
 		yield([idx for idx in grid if idx[0] == i])
 
-	#assert len(row_units) == np.square(size)
-	#return row_units
 
-# Return a list of all columns
+# Yield a list of all columns
 def column_units(size=3):
 	grid = get_grid(size)
-	#col_units = []
 	for i in range(np.square(size)):
-		#col_units.append([idx for idx in grid if idx[1] == i])
 		yield([idx for idx in grid if idx[1] == i])
 
-	#assert len(col_units) == np.square(size)
-	#return col_units
 
-# Return a list of all squares
+# Yield a list of all squares
 def square_units(size=3):
 	enum = range(size)
 	key_grid = [g for g in it.product(enum, enum)]
 
 	# Make first column of boxes
-	col = [[(kg1 + i, kg2) for (kg1, kg2) in key_grid]
+	cols = [[(kg1 + i, kg2) for (kg1, kg2) in key_grid]
 	 		for i in range(0, np.square(size), size)]
 
-	# Fill out the rest
-	#sqr_units = []
+	# Yield each grid adding appropriate numbers to the rows as necessary.
+	# Note we start from zero in the loop to yield the originally generated
+	# columns in cols[].
 	for i in range(0, np.square(size), size):
-		grid = [[(c1, c2 + i) for (c1, c2) in c] for c in col]
-		for g in grid:
-			yield(g)
-			#sqr_units.append(g)
+		# For each col in column: return a list of coordinate tuples with value
+		# i appended to the row.
+		grids = [[(c1, c2 + i) for (c1, c2) in col] for col in cols]
+		for grid in grids:
+			yield(grid)
 
-
-	#assert len(sqr_units) == np.square(size)
-	#return sqr_units
 
 # Return a list of all rows, cols, and sqrs
 def unit_list(size=3):
@@ -75,5 +62,8 @@ def peers(size=3):
 	 							np.square(size) - (2 * size - 1)
 
 	return peers
-
-print(peers(3)[(0,0)])
+"""
+#print(peers(3)[(0,0)])
+for g in square_units():
+	print(g)
+"""
--- solver.py
+++ solver.py
@@ -3,28 +3,63 @@ import numpy as np
 
 class Solver:
 	def __init__(self, size=3):
+		"""
+		Initialize a solver instance.
+		Args:
+			size(int) - Specifies the size of the sudoku puzzle by inner length.
+				   That is to say by the length of the sides of the small squares of the puzzle.
+		"""
+		# Save size for later reference
 		self.size = size
+		# Get the list of all units from common.unit_list()
 		self.unitlist = common.unit_list(size)
+		# Get the peers dictionary from common.unit_list()
 		self.peers = common.peers(size)
+		# Initialize assignments to blank list
 		self.assignments = []
-		self.vals = [v for v in range(1, size*size + 1)]
+		# Initialize the values reference to 1 to size * size inclusive range
+		self.values = [v for v in range(1, size*size + 1)]
+		# Initialize the coordinates reference for the 0 to size * size - 1 exclusive range.
+		# Used to iterate through all rows/columns in self.display()
 		self.coordinates = [c for c in range(0, size*size)]
+		# Get a list of all boxes
 		self.boxes = common.get_grid(size)
 
+
 	def grid_values(self, grid):
+		"""
+		Convert string input into a new puzzle grid. Store puzzle grid as
+			self.assignmens.
+		Args:
+			grid(string) - String for initial puzzle with blanks as '0' or '.'
+		Returns:
+			Dictionary representing the puzzle with real and possible values.
+		"""
+
 		values = []
+		assert len(grid) == np.power(self.size, 4), \
+			"Grid is not long enough. Returns length: %r" % len(grid)
+		# For each character in the grid string: fill values with self.values if
+		# the character is '.' or '0', else just use the character
 		for box in grid:
 			if box == '0' or box == '.':
-				values.append(self.vals.copy())
-			elif int(box) in self.vals:
+				values.append(self.values.copy())
+			elif int(box) in self.values:
 				values.append([int(box)])
 
-		assert len(values) == np.power(self.size, 4), \
-										"len(values) = %r  size = %r" % (len(values), np.square(self.size))
-		self.assignments = values
-		return dict(zip(self.boxes, values))
+		self.assignments = dict(zip(self.boxes, values))
+		return self.assignments
+
 
 	def search(self, values=None):
+		"""
+		Search for a puzzle solution by trial and error.
+		Args:
+			values(dict) - The puzzle dictionary to search with.
+					 Will default to self.assignments if left as None
+		Returns:
+			A solved puzzle dictionary.
+		"""
 		if values == None:
 			values = self.assignments
 
@@ -43,104 +78,177 @@ class Solver:
 					  if len(values[box]) > 1)
 
 		for value in values[box]:
-			#print("SEARCHING")
 			puzzle = values.copy()
-			puzzle[box] = value
-			run = self.search(puzzle)
+			puzzle[box] = puzzle[box].remove(value)
 			if run:
 				return run
 
-	def num_possible(self, values=None, length=1):
+
+	def solve_to_length(self, length=1, values=None):
+		"""
+		Returns the number of values of specified length.
+		Args:
+			length(int) - The length to check against
+			values(dict) - The puzzle we wish to check the length in.
+					 Will default to self.assignments if left as None.
+		Returns:
+			The number of boxes of the specified length.
+		"""
 		if values == None:
 			values = self.assignments
 		# Return number of boxes with number of possibilities == length
 		out = []
 		for box in values.keys():
-			if isinstance(values[box], int):
-				print(box)
 			if len(values[box]) == length:
 				out.append(box)
 		return len(out)
 
-	def reduce_puzzle(self, values):
-		stalled = False
+
+	def reduce_puzzle(self, values=None):
+		"""
+		Apply our constraint functions to the puzzle.
+		Args:
+			values(dict) - The puzzle to apply constraint functions to. Will default
+					 to self.assignments if left as None.
+		Returns:
+			values with the constraint funcitons applied or False if our
+				constraint functions deleted a box completely.
+		"""
+
 
 		if isinstance(values, str):
 			values = self.grid_values(values)
 
+		stalled = False
+
 		while not stalled:
-			solved_before = self.num_possible(values)
+			solved_before = self.solve_to_length(values)
 			print("Solved before: ", solved_before)
 
 			values = self.eliminate(values)
-			print([key for key in values.keys() if isinstance(values.keys(),int)])
 			values = self.only_choice(values)
-			print([key for key in values.keys() if isinstance(values.keys(),int)])
 			values = self.naked_twins(values)
-			print([key for key in values.keys() if isinstance(values.keys(),int)])
 
-			solved_after = self.num_possible(values)
+			solved_after = self.solve_to_length(values)
 			print("Solved after: ", solved_after)
 
 			stalled = solved_before == solved_after
-			if self.num_possible(values, 0) > 0:
+			if self.solve_to_length(values, 0) > 0:
 				return False
 
 		return values
 
-	def eliminate(self, values = None):
+
+	def eliminate(self, values=None):
+		"""
+		Remove values from a box's peers if the box is solved (i.e. of length 1)
+		Args:
+			values(dict) - The puzzle to apply the function to.
+			 			   Will default to self.assignments if left as None.
+		Returns:
+			values with legal possibilites (all solved values are unique within
+				their unit).
+		"""
 		if values == None:
 			values = self.assignments
+
 		solved = [key for key in values.keys() if len(values[key]) == 1]
 		for slv in solved:
 			for peer in self.peers[slv]:
 				assert len(values[slv]) != 0
 				assert len(values[slv]) == 1
 				for v in values[slv]:
-					if v in values[peer] and len(values[peer]) > 1:
+					if v in values[peer]:
+						length_before = len(values[peer])
 						values[peer].remove(v)
+						assert len(values[peer]) < length_before
+						assert not isinstance(values[peer], int)
 
 		return values
 
-	def only_choice(self, values = None):
+	def only_choice(self, values=None):
+		"""
+		Assign a value to a box if that box is the only place the value appears
+			within its unit.
+		Args:
+			values(dict) - The puzzle to apply the functions to.
+						   Will default to self.assignments if left as None.
+		Returns:
+			values with unique values assigned if they are unique to their unit.
+		"""
 		if values == None:
 			values = self.assignments
 
 		for unit in self.unitlist:
-			for val in self.vals:
-				place = []
-				place = [box for box in unit if len(values[box]) > 1 and val in values[box]]
-				if len(place) == 1:
-					print("only")
+			for val in self.values:
+				# For each possible value, record each place the value occurs in a
+				# unit. If the value only appears once, finalize the value
+				place = [box for box in unit if val in values[box]]
+				if len(place) == 1 and len(values[place[0]]) > 1:
+					length_before = len(values[place[0]])
 					values[place[0]] = [val]
+					assert len(values[place[0]]) < length_before
+					assert not isinstance(values[peer], int)
 
 		return values
 
-	def naked_twins(self, values=None):
-		if values == None:
-			values = self.assignments
-		# Find all instances of naked twins
+	def find_twins(self, values):
+		"""
+		Collect a list of unique twin pairs within values.
+		Args:
+			values(dict) - The puzzle to find twins in.
+		Returns:
+			A list of tuples of unique twin pairs within values.
+		"""
 		pairlist = [box for box in values.keys() if len(values[box]) == 2]
 		twinlist = []
-		for box1 in pairlist:
-			for box2 in self.peers[box1]:
-				#print('Naked: {} {}'.format(box1, box2))
-				#print('Twins: {} {}'.format(values[box1], values[box2]))
-				if set(values[box1]) == set(values[box2]) and (box2, box1) not in twinlist:
-					twinlist.append((box1, box2))
-					print("Twinlist: {}".format(twinlist))
+		for pair in pairlist:
+			for peer in self.peers[pair]:
+				# If the sets are equivalent and the reverse tuple is not already
+				# in the twinlist add the tuple
+				if set(values[pair]) == set(values[peer]) and (peer, pair) not in twinlist:
+					twinlist.append((pair, peer))
+					print("Twinlist: ", twinlist)
+
+		return twinlist
+
+
+	def naked_twins(self, values=None):
+		"""
+		Remove twin values from the mutual peers of naked twins.
+		Args:
+			values(dict) - The puzzle to apply the function to. Will default
+					 to self.assignments if left as None.
+		Returns:
+			values with legal possibilites (all solved values are unique within
+				their unit).
+		"""
+		if values == None:
+			values = self.assignments
+
+		# Find all instances of naked twins
+		twinlist = self.find_twins(values)
 
 		# Eliminate the naked twins as possibilities for their peers
 		for twins in twinlist:
 			# get only mutual peers
 			mutual = set(self.peers[twins[0]]) & set(self.peers[twins[1]])
+
+			# Since we know twins[0] and twins[1] are equal
 			twin = twins[0]
 			print('Twins')
+
+			# For each mutual peer: if the peer box contians more than one value, and
+			# the peer box is not in our twinlist then remove the values
+			# of our naked twins from that peer
 			for peer in mutual:
-				if len(values[peer]) > 2:
-					for i in values[twin]:
-						self.assign_value(values, peer, values[peer].replace(i, ''))
-		#print(values)
+				if len(values[peer]) > 1 and peer not in twinlist:
+					for val in values[twin]:
+						if val in values[peer]:
+							values[peer].remove(val)
+							assert len(values[peer]) < length_before
+							assert not isinstance(values[peer], int)
+
 		return values
 
 	# Returns the indexs of square units
@@ -150,6 +258,13 @@ class Solver:
 		return range(size - 1, size*size - size, size)
 
 	def display(self, values=None):
+		"""
+		Display the solved puzzle (or any puzzle really).
+		Args:
+			values(dict) - The puzzle to display.
+		Returns:
+			None
+		"""
 		if values == None:
 			values = self.assignments
 
@@ -173,20 +288,5 @@ class Solver:
 if __name__ == '__main__':
 	s = Solver(3)
 
-	#set_diagonal_mode(True)
-	"""
 	diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
 	s.search(diag_sudoku_grid)
-	"""
-	grid1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
-	grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
-	s.display(s.search(grid2))
-
-	try:
-		from visualize import visualize_assignments
-		visualize_assignments(assignments)
-
-	except SystemExit:
-		pass
-	except:
-		print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
